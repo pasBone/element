496f7272779a8c162f9ac21522f7b4487ead2931
pasbone <pasbone2@gmail.com>
2018-11-08 17:45:37 +0800

    build lib files

--- packages/upload/src/index.vue
+++ packages/upload/src/index.vue
@@ -1,13 +1,13 @@
 <script>
-import UploadList from './upload-list';
-import Upload from './upload';
-import ElProgress from 'element-ui/packages/progress';
-import Migrating from 'element-ui/src/mixins/migrating';
+import UploadList from "./upload-list";
+import Upload from "./upload";
+import ElProgress from "element-ui/packages/progress";
+import Migrating from "element-ui/src/mixins/migrating";
 
 function noop() {}
 
 export default {
-  name: 'ElUpload',
+  name: "ElUpload",
 
   mixins: [Migrating],
 
@@ -25,7 +25,7 @@ export default {
 
   inject: {
     elForm: {
-      default: ''
+      default: ""
     }
   },
 
@@ -44,7 +44,7 @@ export default {
     multiple: Boolean,
     name: {
       type: String,
-      default: 'file'
+      default: "file"
     },
     drag: Boolean,
     dragger: Boolean,
@@ -56,7 +56,7 @@ export default {
     accept: String,
     type: {
       type: String,
-      default: 'select'
+      default: "select"
     },
     beforeUpload: Function,
     beforeRemove: Function,
@@ -95,7 +95,7 @@ export default {
     },
     listType: {
       type: String,
-      default: 'text' // text,picture,picture-card
+      default: "text" // text,picture,picture-card
     },
     httpRequest: Function,
     disabled: Boolean,
@@ -126,8 +126,8 @@ export default {
       immediate: true,
       handler(fileList) {
         this.uploadFiles = fileList.map(item => {
-          item.uid = item.uid || (Date.now() + this.tempIndex++);
-          item.status = item.status || 'success';
+          item.uid = item.uid || Date.now() + this.tempIndex++;
+          item.status = item.status || "success";
           return item;
         });
       }
@@ -138,7 +138,7 @@ export default {
     handleStart(rawFile) {
       rawFile.uid = Date.now() + this.tempIndex++;
       let file = {
-        status: 'ready',
+        status: "ready",
         name: rawFile.name,
         size: rawFile.size,
         percentage: 0,
@@ -146,11 +146,11 @@ export default {
         raw: rawFile
       };
 
-      if (this.listType === 'picture-card' || this.listType === 'picture') {
+      if (this.listType === "picture-card" || this.listType === "picture") {
         try {
           file.url = URL.createObjectURL(rawFile);
         } catch (err) {
-          console.error('[Element Error][Upload]', err);
+          console.error("[Element Error][Upload]", err);
           return;
         }
       }
@@ -161,14 +161,14 @@ export default {
     handleProgress(ev, rawFile) {
       const file = this.getFile(rawFile);
       this.onProgress(ev, file, this.uploadFiles);
-      file.status = 'uploading';
+      file.status = "uploading";
       file.percentage = ev.percent || 0;
     },
     handleSuccess(res, rawFile) {
       const file = this.getFile(rawFile);
 
       if (file) {
-        file.status = 'success';
+        file.status = "success";
         file.response = res;
 
         this.onSuccess(res, file, this.uploadFiles);
@@ -179,7 +179,7 @@ export default {
       const file = this.getFile(rawFile);
       const fileList = this.uploadFiles;
 
-      file.status = 'fail';
+      file.status = "fail";
 
       fileList.splice(fileList.indexOf(file), 1);
 
@@ -199,7 +199,7 @@ export default {
 
       if (!this.beforeRemove) {
         doRemove();
-      } else if (typeof this.beforeRemove === 'function') {
+      } else if (typeof this.beforeRemove === "function") {
         const before = this.beforeRemove(file, this.uploadFiles);
         if (before && before.then) {
           before.then(() => {
@@ -220,32 +220,40 @@ export default {
       return target;
     },
     abort(file) {
-      this.$refs['upload-inner'].abort(file);
+      this.$refs["upload-inner"].abort(file);
     },
     clearFiles() {
       this.uploadFiles = [];
     },
     submit() {
-      this.uploadFiles
-        .filter(file => file.status === 'ready')
-        .forEach(file => {
-          this.$refs['upload-inner'].upload(file.raw);
-        });
+      this.uploadFiles.filter(file => file.status === "ready").forEach(file => {
+        this.$refs["upload-inner"].upload(file.raw);
+      });
     },
     getMigratingConfig() {
       return {
         props: {
-          'default-file-list': 'default-file-list is renamed to file-list.',
-          'show-upload-list': 'show-upload-list is renamed to show-file-list.',
-          'thumbnail-mode': 'thumbnail-mode has been deprecated, you can implement the same effect according to this case: http://element.eleme.io/#/zh-CN/component/upload#yong-hu-tou-xiang-shang-chuan'
+          "default-file-list": "default-file-list is renamed to file-list.",
+          "show-upload-list": "show-upload-list is renamed to show-file-list.",
+          "thumbnail-mode":
+            "thumbnail-mode has been deprecated, you can implement the same effect according to this case: http://element.eleme.io/#/zh-CN/component/upload#yong-hu-tou-xiang-shang-chuan"
         }
       };
+    },
+    moveIndex(start, end) {
+      //新增move方法
+      if (start > end) {
+        start = end;
+        end = start;
+      }
+      this.uploadFiles.splice(end, 0, this.uploadFiles.splice(start, 1)[0]);
+      return this.uploadFiles;
     }
   },
 
   beforeDestroy() {
     this.uploadFiles.forEach(file => {
-      if (file.url && file.url.indexOf('blob:') === 0) {
+      if (file.url && file.url.indexOf("blob:") === 0) {
         URL.revokeObjectURL(file.url);
       }
     });
@@ -261,8 +269,8 @@ export default {
           listType={this.listType}
           files={this.uploadFiles}
           on-remove={this.handleRemove}
-          handlePreview={this.onPreview}>
-        </UploadList>
+          handlePreview={this.onPreview}
+        />
       );
     }
 
@@ -272,8 +280,8 @@ export default {
         drag: this.drag,
         action: this.action,
         multiple: this.multiple,
-        'before-upload': this.beforeUpload,
-        'with-credentials': this.withCredentials,
+        "before-upload": this.beforeUpload,
+        "with-credentials": this.withCredentials,
         headers: this.headers,
         name: this.name,
         data: this.data,
@@ -283,16 +291,16 @@ export default {
         listType: this.listType,
         disabled: this.uploadDisabled,
         limit: this.limit,
-        'on-exceed': this.onExceed,
-        'on-start': this.handleStart,
-        'on-progress': this.handleProgress,
-        'on-success': this.handleSuccess,
-        'on-error': this.handleError,
-        'on-preview': this.onPreview,
-        'on-remove': this.handleRemove,
-        'http-request': this.httpRequest
+        "on-exceed": this.onExceed,
+        "on-start": this.handleStart,
+        "on-progress": this.handleProgress,
+        "on-success": this.handleSuccess,
+        "on-error": this.handleError,
+        "on-preview": this.onPreview,
+        "on-remove": this.handleRemove,
+        "http-request": this.httpRequest
       },
-      ref: 'upload-inner'
+      ref: "upload-inner"
     };
 
     const trigger = this.$slots.trigger || this.$slots.default;
@@ -300,14 +308,12 @@ export default {
 
     return (
       <div>
-        { this.listType === 'picture-card' ? uploadList : ''}
-        {
-          this.$slots.trigger
-            ? [uploadComponent, this.$slots.default]
-            : uploadComponent
-        }
+        {this.listType === "picture-card" ? uploadList : ""}
+        {this.$slots.trigger
+          ? [uploadComponent, this.$slots.default]
+          : uploadComponent}
         {this.$slots.tip}
-        { this.listType !== 'picture-card' ? uploadList : ''}
+        {this.listType !== "picture-card" ? uploadList : ""}
       </div>
     );
   }
